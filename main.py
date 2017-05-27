1a9c18c8b89c5a0cb60c0a98651e2a1d5ebf3b62
Clayton Meyer <sploing1@gmail.com>
2017-05-27 15:11:36 -0700

    simple question and answer

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 lines = [line.rstrip('\n') for line in open('answer_file.test')]
+for x in range(0, len(lines)):
+    if lines[x].endswith('-'):
+        print((lines[x])[:-1])
+        correct = x
+    else:
+        print(lines[x])
+if int(input()) == correct:
+    print("Correct")
+else:
+    print("Incorrect")
